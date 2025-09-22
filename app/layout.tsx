2dc0177a9206225464bea44212656ee780163f10
LucasSovre <lucas.sovre@proton.me>
2025-09-22 20:55:16 +0200

    attempt to include the script

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import {Toaster} from "react-hot-toast";
 import { DM_Sans } from 'next/font/google'
 import CoreBanner from "@/components/ui/coreBanner";
 import Instrumentation from "@/components/instrumentation";
+import Script from "next/script";
 
 const dm_sans = DM_Sans({ subsets: ['latin'] })
 
@@ -50,6 +51,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      {!process.env.DISABLE_TELEMETRY && 
+        <Script
+          src="https://opentech-ux.org/lom-captor/dist/opentech-ux-lib.js"
+          strategy="beforeInteractive"
+          async
+          data-endpoint="https://cattlemoontwelve.ux-key.com/endpoint"
+          suppressHydrationWarning
+        />
+        }
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${dm_sans.className} antialiased h-screen`}
       >

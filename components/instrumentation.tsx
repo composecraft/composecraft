d3a6103eef272ef3c3a3db001271d5800e1c6d68
LucasSovre <lucas.sovre@proton.me>
2025-10-03 18:11:22 +0200

    add ux-key analytics

--- components/instrumentation.tsx
+++ components/instrumentation.tsx
@@ -15,7 +15,5 @@ export default function Instrumentation({posthogKey}:{posthogKey:string}){
     });
     },[posthogKey])
 
-    return <>
-        <script async src="https://opentech-ux.org/lom-captor/dist/opentech-ux-lib.js" data-endpoint="https://drovestationdawn.ux-key.com/endpoint"></script>
-    </>
+    return <></>
 }
